a6d49ec5758cba6d6733d4074b78f145ec7c52a6
Taine Rasmussen <82087605+taine-rasmussen@users.noreply.github.com>
2026-02-25 14:29:00 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import Button from "primevue/button";
+import Card from './components/Card.vue';
+
 </script>
 
 <template>
   <div>
     <h1>the_sandbox</h1>
     <Button label="Click me" />
+    <Card />
   </div>
 </template>
 
